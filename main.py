41081cedef8f3360a9a7c5316bd9defd18629f7c
Joël Treichler <joel@jtreichler.ch>
2020-12-06 14:31:22 +0100

    Update

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ def manual_receipt():
 
 
 
-        return render_template("upload-receipt.html", form=form, form2=form2, showform3 = showform3, form3 = form3)
+        return render_template("upload-receipt.html", form2=form2, showform3 = showform3, form3 = form3)
     else:
         return redirect(url_for("login"))
 
